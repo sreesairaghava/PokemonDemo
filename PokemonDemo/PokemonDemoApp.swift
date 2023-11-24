ad6db90e77c156b06a32cf77c33fdd71faf92f35
Sree Sai Raghava <raghava.rym@gmail.com>
2023-11-24 14:11:56 +0530

    Initial Commit

---
+++ PokemonDemo/PokemonDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonDemoApp.swift
+//  PokemonDemo
+//
+//  Created by Sree Sai Raghava Dandu on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
